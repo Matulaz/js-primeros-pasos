f0ee1c2dccd5a4ae152cebcfab654a74d809d881
matulaz <matiaszarate.psi@gmail.com>
2023-02-26 22:13:05 -0300

    respuestas comentadas en app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+// EJERCICIO 3
+// let tieneHijos = prompt("Ingrese si o no, para saber si tiene hijos")
+
+// tieneHijos == "si" ? tieneHijos = "true" : tieneHijos = "false"
+
+// console.log(tieneHijos)
+
+// 10
+// let num = prompt("Ingrese un numero positovo del 1 al 99");
+
+// if (num >= 1 && num <= 9){
+//     console.log("El numero ingresado tiene un digito")
+// }else if ( num > 9 && num <= 99){
+//     console.log ("el numero ingresado tiene dos digitos")
+// }else{
+//     console.log("El valor ingresado no es correcto")
+// }
+
+
+// EJERCICIO 13
+// let consultaDinero = prompt("Indique 1 para pagar con Efectivo, 2 para pagar con Tarjeta y 3  si no cuenta con ninguno de estos medios de pago")
+
+// if (consultaDinero == 1 || consultaDinero == 2){
+//     console.log("Felicitaciones! Puedes pagar")
+// } else {
+//     console.log("No tienes suficiente dinero")
+// }
+
+// EJERCICIO 16
+// let nombre = prompt("Ingrese su nombre ", "Ejemplo: Juan Perez")
+
+// let sueldo = parseInt(prompt("Ingrese su salario actual sin espacios ni signos \nEjemplo: \n200"))
+
+// let antiguedad = prompt ("Ingrese la cantidad de años que hace que trabaja aqui \n Indique \n 1 - Si trabaja hace 10 Años o mas \n 2 - Si trabaja hace menos de 10 años")
+
+
+// if (sueldo < 500 && antiguedad == "1"){
+//     console.log(`${nombre} recibira un aumento del 20% de su sueldo, a partir del proximo mes su sueldo sera de $ ${sueldo * 1.2}`)
+// } else if (sueldo < 500 && antiguedad == "2"){
+//     console.log(`${nombre} recibira un aumento del 5% de su sueldo, a partir del proximo mes su sueldo sera de $ ${sueldo * 1.05}`)
+// }else{
+//     console.log(`${nombre} su sueldo actual es de $ ${sueldo}`)
+// }
+
